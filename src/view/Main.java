cceb870891fdd0e7088c69eb354071b973241767
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2020-11-26 10:33:44 -0500

    el main también

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,10 +1,24 @@
 package view;
 
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PApplet.main("view.Main");
+	}
+	
+	public void settings() {
+		
+	}
+	
+	public void setup() {
+		
 	}
 
+	public void draw() {
+		
+	}
 }
